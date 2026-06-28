18986a9893484523f44c455e874c4e78dfe60735
Rahul-R-007 <sreerahul67@gmail.com>
2026-06-28 14:58:31 +0530

    database file

---
+++ database.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="rahul",
+    password="Rahul@123",
+    database="telegram_bot"
+)
+
+cursor = db.cursor()
+
+print("Connected to MySQL successfully!")
+
+def save_pdf(chat_id, message_id, file_name, raw_text):
+    sql = """
+    INSERT INTO pdf_data (
+        telegram_chat_id,
+        telegram_message_id,
+        file_name,
+        raw_text
+    )
+    VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(sql, (
+        chat_id,
+        message_id,
+        file_name,
+        raw_text
+    ))
+
+    db.commit()
+
+    return cursor.lastrowid
